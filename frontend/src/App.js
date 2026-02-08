5078f90fc60897950bb812b0a2727cff8484a2b2
jinopattery <76552140+jinopattery@users.noreply.github.com>
2026-02-09 00:03:42 +0100

    Update App.js to add routes for FTADetail and StructureAnalysis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import FTADetail from './pages/FTADetail';
 import StructureAnalysis from './pages/StructureAnalysis';
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/structure/:id" component={StructureAnalysis} />
-                {/* Other routes can go here */}
-            </Switch>
-        </Router>
+        <div>
+            <nav>
+                <Link to="/structure/1">Structure</Link>
+            </nav>
+            <Routes>
+                <Route path="/fta/:id" element={<FTADetail />} />
+                <Route path="/structure/:id" element={<StructureAnalysis />} />
+            </Routes>
+        </div>
     );
 }
 
